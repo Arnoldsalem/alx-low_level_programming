e612c34c98171cb3c9b3b48ff19e92ae003f6f73
Arnoldsalem <arnoldsalem@gmail.com>
2023-09-12 14:43:23 -0700

    task98batterystreet

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,24 @@
 #include "main.h"
 /**
-  * print_to_98
-  @n: print integers
-  * Return: 
-  *
+  * print_to_98 - print all natural numbers
+  * @n: input number
+  * Return: no return
+  */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d/n", n);
+}
