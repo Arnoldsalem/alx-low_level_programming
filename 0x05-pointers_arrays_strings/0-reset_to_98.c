5c359001e31ecff37c5a14a8b836be41ee37bf6e
Arnoldsalem <arnoldsalem@gmail.com>
2023-09-18 03:05:42 -0700

    pointing to a value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * reset_to_98 - takes a pointer and
+  * uptes the value it points.
+  * @n: int to check
+  * Return: 0 s success
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
