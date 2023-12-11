04e620507c609981379d402f9530422bab84d4ab
Arnoldsalem <arnoldsalem@gmail.com>
2023-12-11 10:55:38 -0800

    task 0 and 1

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concat 2 string
+ * @src: char string 1
+ * @dest: char string 2
+ * Return:char
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+libraries_using_C
